b94c0e989c77f3b5c3a2bbb6e433aa5c463b493e
Irving <red.irving15@gmail.com>
2018-11-02 17:34:43 -0500

    working example of MVT

--- adeluxuryeyewear/ade/urls.py
+++ adeluxuryeyewear/ade/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Home page routes
+
 urlpatterns = [
     path('', views.index, name='index')
 ]
--- adeluxuryeyewear/ade/views.py
+++ adeluxuryeyewear/ade/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Home page view
+def index(request):
+    """ *Test only """
+
+    msg = 'Hello World!'
+
+    # The data sent to the template
+    context = {
+        'message': msg
+    }
+
+    # The return response - the template with the data(context)
+    return render(request, 'ade/index.html', context)
+
