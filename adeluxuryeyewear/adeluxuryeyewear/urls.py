d205503541257cb3bdce0fbdedde27495292f909
Roman Lopez <roman.lopez93@yahoo.com>
2018-11-01 20:13:58 -0500

    added more skeleton files to make it easier for the front-end person to start.

--- adeluxuryeyewear/adeluxuryeyewear/urls.py
+++ adeluxuryeyewear/adeluxuryeyewear/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
+
+# we can change the name of the uris later
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^about/$', views.about),
-    url(r'^contact/$', views.contact)
+    url(r'^contact/$', views.contact),
+    url(r'^products/', include('products.urls')),
+    url(r'^users/', include('users.urls')),
+    url(r'^checkout/', include('checkout.urls')),
+
 ]
