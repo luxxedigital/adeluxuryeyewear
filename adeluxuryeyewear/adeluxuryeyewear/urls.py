5bac045dabf476e8baaa9938ff8461116907ff6b
Irving <red.irving15@gmail.com>
2018-11-02 14:00:35 -0500

    url patterns

--- adeluxuryeyewear/adeluxuryeyewear/urls.py
+++ adeluxuryeyewear/adeluxuryeyewear/urls.py
@@ -1,16 +1,36 @@
+"""root urlConf"""
+
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.urls import include, path
 from . import views
 
 
 # we can change the name of the uris later
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'^about/$', views.about),
-    url(r'^contact/$', views.contact),
-    url(r'^products/', include('products.urls')),
-    url(r'^users/', include('users.urls')),
-    url(r'^checkout/', include('checkout.urls')),
+]
 
+urlpatterns += [
+    path('', views.index, name='index')
+]
+
+urlpatterns += [
+    path('about/', include('about.urls'))
+]
+
+urlpatterns += [
+    path('contact/', include('contact.urls'))
+]
+
+urlpatterns += [
+    path('users/', include('users.urls'))
+]
+
+urlpatterns += [
+    path('products/', include('products.urls'))
+]
+
+urlpatterns += [
+    path('checkout/', include('checkout.urls'))
 ]
