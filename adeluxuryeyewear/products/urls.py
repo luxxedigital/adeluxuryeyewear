07d2d24e4250f7d5298dbed99e2a8264e810f8b3
Irving <red.irving15@gmail.com>
2018-11-02 20:25:50 -0500

    added product routes

--- adeluxuryeyewear/products/urls.py
+++ adeluxuryeyewear/products/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
     url(r'^$', views.products),
+    path('featured-line/', views.featured_line),
+    path('kings-line/', views.kings_line),
+    path('queens-line/', views.queens_line),
+    path('unisex-line/', views.unisex_line),
 ]
--- adeluxuryeyewear/products/views.py
+++ adeluxuryeyewear/products/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def products(request):
     return HttpResponse('products')
+
+def featured_line(request):
+    return HttpResponse("kings-line")
+
+def kings_line(request):
+    return HttpResponse("kings-line")
+
+def queens_line(request):
+    return HttpResponse("kings-line")
+
+def unisex_line(request):
+    return HttpResponse("kings-line")
