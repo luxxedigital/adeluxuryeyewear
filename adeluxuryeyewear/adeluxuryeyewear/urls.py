f06cb9c0389ff9a5f4b9f28d144ec535d273e42b
Irving <red.irving15@gmail.com>
2018-11-02 21:44:01 -0500

    redirect root url to ade/

--- adeluxuryeyewear/adeluxuryeyewear/urls.py
+++ adeluxuryeyewear/adeluxuryeyewear/urls.py
@@ -1,14 +1,16 @@
-"""Root url conf. """
+"""Root URLconf"""
 
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 
 # URL Routes #
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='ade/'), name='index')
 ]
 
 urlpatterns += [
