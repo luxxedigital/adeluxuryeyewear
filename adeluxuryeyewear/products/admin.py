84eba61d71a4c7cf9e721e27d9e5369c4ff19d1b
Irving <red.irving15@gmail.com>
2018-11-15 17:57:13 -0600

    added new models to admin page

--- adeluxuryeyewear/products/admin.py
+++ adeluxuryeyewear/products/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from products.models import Product
-from products.models import Specs
-from products.models import Features
+from products.models import Product, Specs, Features, Review, Rating
 
 admin.site.register(Product)
 admin.site.register(Specs)
 admin.site.register(Features)
+admin.site.register(Review)
+admin.site.register(Rating)
