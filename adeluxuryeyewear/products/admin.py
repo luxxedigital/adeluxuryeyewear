349ce2721bcb800d5d03909225ff8c0d5c096126
Irving <red.irving15@gmail.com>
2018-11-15 03:44:42 -0600

    created the models for collections

--- adeluxuryeyewear/products/admin.py
+++ adeluxuryeyewear/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import Product
+from products.models import Specs
+from products.models import Features
+
+admin.site.register(Product)
+admin.site.register(Specs)
+admin.site.register(Features)
--- adeluxuryeyewear/products/models.py
+++ adeluxuryeyewear/products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# Models for ADE Eyewear products
+
+class Product(models.Model):
+    eyewear_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.CharField(max_length=150)
+    quantity = models.IntegerField()
+    gender_options = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('U', 'Unisex'),
+    )
+    gender = models.CharField(max_length=1, choices=gender_options, default='F')
+    promotional_price = models.FloatField()
+
+class Specs(models.Model):
+    # Specs
+    lens_width = models.CharField(max_length=15)
+    lens_height = models.CharField(max_length=15)
+    bridge_length = models.CharField(max_length=15)
+    arm_length = models.CharField(max_length=15)
+    product_id = models.ForeignKey("Product", on_delete=models.CASCADE)
+    
+
+class Features(models.Model):
+    # Features
+    feature = models.CharField(max_length=50)
+    product_id = models.ForeignKey("Product", on_delete=models.CASCADE)
