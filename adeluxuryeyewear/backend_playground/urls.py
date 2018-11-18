c84a65fd5a08c995a2c457536f6e960f24eea675
Irving <red.irving15@gmail.com>
2018-11-18 00:03:51 -0600

    added more filtering options, and added detail data fetching

--- adeluxuryeyewear/backend_playground/urls.py
+++ adeluxuryeyewear/backend_playground/urls.py
@@ -5,11 +5,22 @@ from . import views
 # Add a special routes with a view to experiment with your code
 #################################################################
 
-# Developing products filtering
+# Filter-by routes
 urlpatterns = [
-    path('filter/', views.view_all),
-    path('filter/all', views.view_all, name='view-all'),
-    path('filter/men', views.view_men, name='view-men'),
-    path('filter/women', views.view_women, name='view-women'),
-    path('filter/unisex', views.view_unisex, name='view-unisex'),
+    path('collection/', views.view_all),
+    path('collection/all', views.view_all, name='collection-all'),
+    path('collection/men', views.view_men, name='collection-men'),
+    path('collection/women', views.view_women, name='collection-women'),
+    path('collection/unisex', views.view_unisex, name='collection-unisex'),
+]
+
+urlpatterns += [
+    path('collection/<str:line>/priority__lowprice', views.sort_by_price__low, name='sortby-price-low'),
+    path('collection/<str:line>/priority__highprice', views.sort_by_price__high, name='sortby-price-high')
+]
+
+
+# Route for a detail view of the product
+urlpatterns += [
+    path('collection/<int:id>/', views.collection_detail, name='collection-detail')
 ]
--- adeluxuryeyewear/backend_playground/views.py
+++ adeluxuryeyewear/backend_playground/views.py
@@ -1,34 +1,76 @@
 from django.shortcuts import render
 from products.models import Product
 
+
+##############
+# List Views
+##############
+
 def view_all(request):
     """Returns all the objects in the Product model (all glasses)"""
     collection_all = Product.objects.all()
 
-    return render(request, 'backend_playground/filter_collections.html', context={
+    return render(request, 'backend_playground/collection_list.html', context={
         'ade_collection': collection_all
     })
 
 def view_men(request):
-    """Returns all the men eyewear objects from Product model"""
-    collection_men = Product.objects.filter(gender='M')
+    """Returns all the men(King's line) eyewear objects from Product model"""
+    collection_men = Product.objects.filter(line='KL')
 
-    return render(request, 'backend_playground/filter_collections.html', context={
+    return render(request, 'backend_playground/collection_list.html', context={
         'ade_collection': collection_men
     })
 
 def view_women(request):
-    """Returns all the women eyewear objects from Product model"""
-    collection_women = Product.objects.filter(gender='F')
+    """Returns all the women(Queen's line) eyewear objects from Product model"""
+    collection_women = Product.objects.filter(line='QL')
 
-    return render(request, 'backend_playground/filter_collections.html', context={
+    return render(request, 'backend_playground/collection_list.html', context={
         'ade_collection': collection_women
     })
 
 def view_unisex(request):
-    """Returns all the unisex eyewear objects from Product model"""
-    collection_women = Product.objects.filter(gender='U')
+    """Returns all the unisex(Unisex line) eyewear objects from Product model"""
+    collection_unisex = Product.objects.filter(line='UL')
 
-    return render(request, 'backend_playground/filter_collections.html', context={
-        'ade_collection': collection_women
+    return render(request, 'backend_playground/collection_list.html', context={
+        'ade_collection': collection_unisex
+    })
+
+#################
+# Sort-by Views
+#################
+
+def sort_by_price__low(request, line):
+    collection_by_price = Product.objects.filter(line=line).order_by('price')
+
+    return render(request, 'backend_playground/collection_list.html', context={
+        'ade_collection': collection_by_price,
+    })
+
+def sort_by_price__high(request, line):
+    collection_by_price = Product.objects.filter(line=line).order_by('-price')
+
+    return render(request, 'backend_playground/collection_list.html', context={
+        'ade_collection': collection_by_price,
+    })
+
+def sort_by_promo_price(request):
+    pass
+
+def sort_by_alphabetical(request):
+    pass
+
+#################
+# Detail View
+#################
+
+def collection_detail(request, id):
+    collection_item = Product.objects.filter(eyewear_id=id)
+
+    print(collection_item)
+
+    return render(request, 'backend_playground/collection_detail.html', context={
+        'ade_collection_item': collection_item
     })
