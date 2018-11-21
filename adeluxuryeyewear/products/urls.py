82db44b58f7d2fba8e020e500a1723d436680274
Irving <red.irving15@gmail.com>
2018-11-21 17:56:16 -0600

    connected the collection (list/detail/filter) routes with proper app (products)

--- adeluxuryeyewear/products/urls.py
+++ adeluxuryeyewear/products/urls.py
@@ -1,13 +1,29 @@
 from django.conf.urls import url
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.products),
-    path('featured-line/', views.featured_line),
-    path('kings-line/', views.kings_line),
-    path('queens-line/', views.queens_line),
-    path('unisex-line/', views.unisex_line),
-    path('', views.products, name='products'),
-    path('detail/', views.detail, name='detail')
+    path('', RedirectView.as_view(url='all/'), name='products'),
+]
+
+# Filter-by collection
+urlpatterns += [
+    path('all/', views.view_all, name='collection__all'),
+    path('KL/', views.view_men, name='collection__men'),
+    path('QL/', views.view_women, name='collection__women'),
+    path('UL/', views.view_unisex, name='collection__unisex'),
+]
+
+# Sort-by criteria
+urlpatterns += [
+    path('<str:line>/priority__low-price', views.sort_by_price__low, name='sortby_price-low'),
+    path('<str:line>/priority__high-price', views.sort_by_price__high, name='sortby_price-high'),
+    path('<str:line>/priority__low-promo-price', views.sort_by_promo_price__low, name='sortby_promoprice-low'),
+    path('<str:line>/priority__high-promo-price', views.sort_by_promo_price__high, name='sortby_promoprice-high')
+]
+
+# Route for a detail view of the product
+urlpatterns += [
+    path('<int:id>/', views.collection_detail, name='collection__detail')
 ]
--- adeluxuryeyewear/products/views.py
+++ adeluxuryeyewear/products/views.py
@@ -1,25 +1,123 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
-# Create your views here.
-def products(request):
-    return render(request, 'products/products.html')
+##############
+# List Views
+##############
 
-def detail(request):
-    return render(request, 'products/product_detail.html')
+def view_all(request):
+    """Returns all the objects in the Product model (all glasses)"""
+    collection_all = Product.objects.all()
 
-def featured_line(request):
-    return HttpResponse("kings-line")
+    return render(request, 'products/collection_list.html', context={
+        'ade_collection': collection_all,
+        'collection': 'all'
+    })
 
-def kings_line(request):
-    return HttpResponse("kings-line")
+def view_men(request):
+    """Returns all the men(King's line) eyewear objects from Product model"""
+    collection_men = Product.objects.filter(line='KL')
 
-def queens_line(request):
-    return HttpResponse("kings-line")
+    return render(request, 'products/collection_list.html', context={
+        'ade_collection': collection_men,
+        'collection': 'KL'
+    })
 
-def unisex_line(request):
-    return HttpResponse("kings-line")
+def view_women(request):
+    """Returns all the women(Queen's line) eyewear objects from Product model"""
+    collection_women = Product.objects.filter(line='QL')
 
-# just to commit something
+    return render(request, 'products/collection_list.html', context={
+        'ade_collection': collection_women,
+        'collection': 'QL'
+    })
+
+def view_unisex(request):
+    """Returns all the unisex(Unisex line) eyewear objects from Product model"""
+    collection_unisex = Product.objects.filter(line='UL')
+
+    return render(request, 'products/collection_list.html', context={
+        'ade_collection': collection_unisex,
+        'collection': 'UL'
+    })
+
+#################
+# Sort-by Views
+#################
+
+def sort_by_price__low(request, line):
+    """ Sorts a collection by price(low-high). The line is either 'all', 'KL', 'QL', or 'UL'.
+        Return the objects in that collection sorted by that criteria.
+    """
+
+    if line != 'KL' and line != 'QL' and line != 'UL':
+        collection_by_price = Product.objects.all().order_by('price')
+        line = 'all'
+    else:
+        collection_by_price = Product.objects.filter(line=line).order_by('price')
+
+    return render(request, 'products/collection_list.html', context={
+        'ade_collection': collection_by_price,
+        'collection': line
+    })
+
+def sort_by_price__high(request, line):
+
+    if line != 'KL' and line != 'QL' and line != 'UL':
+        collection_by_price = Product.objects.all().order_by('-price')
+        line = 'all'
+    else:
+        collection_by_price = Product.objects.filter(line=line).order_by('-price')
+
+    return render(request, 'products/collection_list.html', context={
+        'ade_collection': collection_by_price,
+        'collection': line
+    })
+
+def sort_by_promo_price__low(request, line):
+
+    if line != 'KL' and line != 'QL' and line != 'UL':
+        collection_by_promo_price = Product.objects.all().order_by('promotional_price')
+        line = 'all'
+    else:
+        collection_by_promo_price = Product.objects.filter(line=line).order_by('promotional_price')
+
+    return render(request, 'products/collection_list.html', context={
+        'ade_collection': collection_by_promo_price,
+        'collection': line
+    })
+
+def sort_by_promo_price__high(request, line):
+
+    if line != 'KL' and line != 'QL' and line != 'UL':
+        collection_by_promo_price = Product.objects.all().order_by('-promotional_price')
+        line = 'all'
+    else:
+        collection_by_promo_price = Product.objects.filter(line=line).order_by('-promotional_price')
+
+    return render(request, 'products/collection_list.html', context={
+        'ade_collection': collection_by_promo_price,
+        'collection': line
+    })
+
+def sort_by_name__ascending(request):
+    pass
+
+def sort_by_name__descending(request):
+    pass
+
+#################
+# Detail View
+#################
+
+def collection_detail(request, id):
+    collection_item = Product.objects.filter(eyewear_id=id)
+
+    print(collection_item)
+
+    return render(request, 'products/collection_detail.html', context={
+        'ade_collection_item': collection_item
+    })
 
 
